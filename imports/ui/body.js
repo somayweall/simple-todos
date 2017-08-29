d1e9bb6f5b84b2e48e70c5f0c75cbda24bc1f179
thesurfaceofthevoid <thesurfaceofthevoid@gmail.com>
2017-08-29 08:40:57 -0400

    Initial commit.

---
+++ imports/ui/body.js
@@ -0,0 +1,48 @@
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
+import { Tasks } from '../api/tasks.js';
+
+import './body.html';
+import './task.js';
+
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
+Template.body.helpers({
+  tasks() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
+    // Show newest tasks at the top
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.text.value = '';
+  },
+
+  'change .hide-completed input'(event, instance) {
+  instance.state.set('hideCompleted', event.target.checked);
+},
+});
